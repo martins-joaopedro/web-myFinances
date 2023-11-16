9abb99d69c00089e786b274d0263cc64b4401d53
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2023-11-16 13:21:16 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 
+import axios from "axios";
+
 function App() {
+
+  //const [data, setData] = useState()
+
+  const api = axios.create({
+    baseURL: "https://api-node-iult.onrender.com" 
+  })
   
+  const fetchData = () => {
+    return api.get("/extenses").then((a) => console.log(a))
+
+  }
+
   return (
     <div>
-      AAAAAAAAAAAAAAAAAAAAAA
-      asasadfsf
+      <button onClick={() => fetchData()}></button>
       
     </div>
   )
